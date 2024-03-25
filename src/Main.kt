e2baacc475fc413a2d0a3b938c9d5cab4e42e014
Student <mutesi-alin>
2024-03-25 08:14:56 +0300

    added file

---
+++ src/Main.kt
@@ -0,0 +1,52 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    sentence("Barnie bakes brown bagelsand buns")
+    area(56,10)
+    isPalindrome("hello word")
+
+
+}
+
+fun sentence (word:String){
+    var word ="Barnie bakes brown bagelsand buns"
+            word.removePrefix("b")
+    println(word)
+
+}
+
+fun new  (numbers:Int){
+    val statistics= arrayOf(12,34,76,78,90,34,14,23)
+    println(statistics(average))
+    println(sum().toInt/statistics.size)
+    println(statistics.count()
+}
+
+
+fun area(radius:Int,pi:Int){
+
+    var volume= 4/3π r3
+    println(volume)
+
+}
+fun isPalindrome(word: String): Boolean{
+
+    if ("string, is palindrome"){
+        println(true)
+    }
+    else println(false)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
